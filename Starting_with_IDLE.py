7e6bffe31a1b8de5b74776b7d449b5f1cd856b96
Zuzana Dostálová <zuzanadostalova@Zuzana-Air.homenet.telecomitalia.it>
2020-01-05 18:16:10 +0100

    ...

--- Starting_with_IDLE.py
+++ Starting_with_IDLE.py
@@ -1,5 +1,35 @@
+# Section 1: Course introduction
+# 1. lesson - Lesson structure
+
+# Section 2: Installing Python, getting started
+# 2. lesson - Installing Python on Windows
+# 3. lesson - Installing Python on Mac
+# 4. lesson - Installing Python on Linux
+
 # new github repository
 # in VSC open the terminal - cmd + shift + P, Git: Clone, insert URL, choose location on PC
 
-# 5. lesson - IDLE
+# 5. lesson - Meeting IDLE
 # Python shell - Help - Python docs
+# they do not use VSC
+# starting with print functions
+
+# 6. lesson - The secret of programmers
+# ask good questions -  QA, in the question title write [HELP], then [SOLVED]
+# 1) Observed behaviour, 2) Expected behaviour, 3) Link to code - github, 4) where i think the problem is
+# 5) What I have tried, 6) Errors and warnings, 7) Version info, 8) Screenshot - N/A 
+# not required
+
+# use the community
+
+# 7. lesson - Note  about gist.github.com
+
+# Section 3: Variables
+# 8. lesson - Overview on starting with Python
+
+# 9. lesson
+# variable - stores a value,  little box
+# in Java and C its type has to be defined since the start
+# name = value ... name of the variable on the left, value on the right 
+
+# 10. lesson

47cab74f7e38cef49319327e489e3c901c9f6fea
Zuzana Dostálová <zuzanadostalova@Zuzana-Air.homenet.telecomitalia.it>
2020-01-06 21:26:02 +0100

    ...

---
+++ Section_4_Numbers.py
@@ -0,0 +1,34 @@
+# Section 4: Numbers in Python
+# 12. lesson - Overview of the section Numbers
+# comp will do math for you, learn how to control the order of the computations,
+# learn about floats
+
+# make a health potion for a video game character
+# use numbers to create difficulty system
+# learn about using External Python modules
+
+# 13. lesson - Basic Arithmetic, Floats, and Modulo
+# Basic mathematical operations, Modulo function, the float variable type,
+# Learn the difference between Python 2 and 3
+# mathematical operator - symbol performing mathematical operations 
+# Python 2 - division gives an integer input whereas Py3 gives a float (fixed many problems)
+# type(0.5) 
+# <class "float"> - higher precision with float taking more memory
+
+# Modulo - %, shift 5 on Windows, shift = Mac
+# 5 % 3; the output is 2 ... 5 / 3 = three will go to five once with remainder of two 
+# 10 % 2 the output is 0 ... 10 / 2 = two will go to two twice with remainder of zero
+
+# 14. lesson - Ordering Operations
+# BODMAS - ordering system, closer to B - higher priority
+# Brackets () - executed first
+# Order - raising number to power of something else
+# Division
+# Multiplication
+# Addition
+# Subtraction
+
+# the correct example: 2 * (5-1) = 8
+
+# 15. lesson - Crafting a health potion
+# 
--- Starting_with_IDLE.py
+++ Starting_with_IDLE.py
@@ -3,11 +3,12 @@
 
 # Section 2: Installing Python, getting started
 # 2. lesson - Installing Python on Windows
-# 3. lesson - Installing Python on Mac
-# 4. lesson - Installing Python on Linux
 
+# 3. lesson - Installing Python on Mac
 # new github repository
 # in VSC open the terminal - cmd + shift + P, Git: Clone, insert URL, choose location on PC
+ 
+# 4. lesson - Installing Python on Linux
 
 # 5. lesson - Meeting IDLE
 # Python shell - Help - Python docs
@@ -27,9 +28,26 @@
 # Section 3: Variables
 # 8. lesson - Overview on starting with Python
 
-# 9. lesson
+# 9. lesson - What is variable?
 # variable - stores a value,  little box
 # in Java and C its type has to be defined since the start
 # name = value ... name of the variable on the left, value on the right 
 
-# 10. lesson
+# 10. lesson - Updating our script
+# it is not possible to write 1a = 1, only a1 = 1 and keep them lowercase, without spaces
+
+# variables - for calculations, you can easily add new numbers = shortcut
+# appropriate naming - readability
+# variables are keeping the code organised, easy to search in, and easier to make changes
+
+# test 1. to revise the learnt information
+# Which of these functions is used to see a variables type? 
+# type()
+
+# Python is a "dynamically typed" language. What does this mean? 
+# The variable type can be changed throughout the program.
+
+# coding challenge
+
+# 11. lesson - Overview of the section Variables
+

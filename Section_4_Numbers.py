2d678e67e08f5ef8aa4f44939a6bd5314b57391e
Zuzana Dostálová <zuzanadostalova@Zuzana-MacBook-Air.local>
2020-01-08 19:33:19 +0100

    ...

--- Section_4_Numbers.py
+++ Section_4_Numbers.py
@@ -48,10 +48,77 @@
 # all_health = potion_health + health
 # print(all_health)
 
-# Python math module
+# 17. lesson - Python math module
 # rounding numbers to the nearest integer
-result = round(1.5)
-print(result)
+round(1.5)
+print(round(1.5))
 
 # import math module
-import math module
+import math
+
+# force round down - to the floor
+math.floor(1.5)
+print(math.floor(1.5))
+
+# force round up - up to the ceiling
+math.ceil(2.1)
+print(math.ceil(2.1))
+
+# pi = 3.141592653589793
+math.pi
+print(math.pi)
+
+# exponential = 2.718281828459045
+math.e
+print(math.e)
+
+# build-in trigonometrical functions - radians
+math.sin(math.pi/2)
+print(math.sin(math.pi/2))
+
+math.sin(math.pi)
+print(math.sin(math.pi))
+
+math.floor(math.sin(math.pi))
+print(math.floor(math.sin(math.pi)))
+
+math.cos(0)
+print(math.cos(0))
+
+math.asin(0)
+print(math.asin(0))
+
+math.acos(0)
+print(math.acos(0))
+
+# Output is 5.0; Square 3 = 9, square 4 = 16, 16 + 9 = 25, square root of 25 is 5
+math.hypot(3,4)
+print(math.hypot(3,4))
+
+# Output is 8.0; 2 to the power of 3
+math.pow(2,3)
+print(math.pow(2,3))
+
+# Also build-in operations 2**3 gives the output 8 but in this case it is integer
+
+# 9 to the power of 2
+math.pow(9,2)
+print(math.pow(9,2))
+
+# e to the power of 2
+math.exp(2)
+print(math.exp(2))
+
+# natural log of exponential = 1
+math.log(math.e)
+print(math.log(math.e))
+
+# based 10 log
+math.log10(1000)
+print(math.log10(1000))
+
+math.log2(8)
+print(math.log2(8))
+
+# 19. lesson - Section overview
+# for data analysis - pandas, NumPy, SciPy

c4c1a4a1bc8ba15d96d5b6a86528b7da3b738b1f
Zuzana Dostálová <zuzanadostalova@dhcp-74-064.eduroam.muni.cz>
2020-01-09 09:41:58 +0100

    ....

--- Section_5_Strings.py
+++ Section_5_Strings.py
@@ -1,4 +1,36 @@
 # 19. lesson - Section overview
 # for data analysis - pandas, NumPy, SciPy
 
-# 20. lesson - Hello world
+# 20., 21., and 22. lesson - Hello world
+print("hello")
+name = "Ziyad"
+print(type(name))
+
+# message = "John said "See you later""
+# how to correct this faulty string? ->
+message = 'John said "See you later"'
+
+# How to fit a long text into one variable? Triple quotes either ''' or """
+# punctuation inside does not matter - there can be a double quote etc 
+romeo = """Two households, both alike in dignity,
+In fair Verona, where we lay our scene,
+From ancient grudge break to new mutiny,
+Where civil blood makes civil hands unclean.
+From forth the fatal loins of these two foes
+A pair of star-cross'd lovers take their life;
+Whose misadventured piteous overthrows
+Do with their death bury their parents' strife.
+The fearful passage of their death-mark'd love,
+And the continuance of their parents' rage,
+Which, but their children's end, nought could remove,
+Is now the two hours' traffic of our stage;
+The which if you with patient ears attend,
+What here shall miss, our toil shall strive to mend."""
+print(romeo)
+
+# Hello world
+hello = "Hello world!"
+print(hello)
+
+# gathering input, combine various typr of data to make meaningful messages, to create a program
+# taking user input, processing it, and giving an output

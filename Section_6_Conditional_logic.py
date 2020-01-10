843e81ce90ca49661ed3abe35e1d0bab8cf7b864
Zuzana Dostálová <zuzanadostalova@Zuzana-MacBook-Air.local>
2020-01-10 18:47:09 +0100

    ...

--- Section_6_Conditional_logic.py
+++ Section_6_Conditional_logic.py
@@ -11,11 +11,34 @@ print(2>3)
 print(type(2<3))
 # Output: <class 'bool'>
 
-2 = 3
+# print(2 = 3)
 # Output: Error - cannot assign to literal
 
-2 == 3
+print(2 == 3)
 # Output: False
 
-3 == 3
+print(3 == 3)
 # Output: True
+
+print(2 != 3)
+# Output: True; 2 does not equal to 3
+
+print(4 >= 3)
+# Output: True, 4 is bigger or equals 3
+
+print(3 >= 3)
+# Output: True
+
+print(2 >= 3)
+# Output: False
+
+print(4 <= 3)
+# Output: False
+
+print(2 <= 3)
+# Output: True
+
+print(3 <= 3)
+# Output: True
+
+# 6 booleans >, <, ==, !=, >=, <=

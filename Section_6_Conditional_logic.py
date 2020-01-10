34b24ddf29dee5f207875fc0a438f653561a3e12
Zuzana Dostálová <zuzanadostalova@Zuzana-MacBook-Air.local>
2020-01-10 18:08:08 +0100

    ...

---
+++ Section_6_Conditional_logic.py
@@ -0,0 +1,21 @@
+# 30. lesson - Future lesson overview
+
+# 31. lesson - Booleans
+# Boolean is not created through typing its value - we get it from doing logical comparison
+print(2<3)
+# Output: True
+
+print(2>3)
+# Output: False
+
+print(type(2<3))
+# Output: <class 'bool'>
+
+2 = 3
+# Output: Error - cannot assign to literal
+
+2 == 3
+# Output: False
+
+3 == 3
+# Output: True

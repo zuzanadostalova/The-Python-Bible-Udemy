87bceb1c623df1890c7609b68db31501c2a7d615
Zuzana Dostálová <zuzanadostalova@Zuzana-MacBook-Air.local>
2020-01-14 13:25:48 +0100

    ...

--- 7_Cinema_simulator.py
+++ 7_Cinema_simulator.py
@@ -1,9 +1,9 @@
 # 45. lesson - Cinema simulator
 films = {
-    "Finding Dory":[3,5],
-    "Bourne":[18,5],
-    "Tarzan":[15,5],
-    "Ghost busters":[12,5]
+    "Finding Dory":[3.5],
+    "Bourne":[18.5],
+    "Tarzan":[15.5],
+    "Ghost busters":[12.5]
 }
 
 # print(films["Finding Dory"])
@@ -13,18 +13,39 @@ while True:
     if answer == "Finding Dory":
         age = input("What is your age? ")  
 
-        # age_limit = []
-        # if age >= age_limit:
+        if int(age) >= 3.5:
             input("How many tickets would you like to buy? ")
-    
+
+        else:
+            print("I am sorry, you cannot buy any ticket.")
+
+
     elif answer == "Bourne":
         age = input("What is your age? ")
+        
+        if int(age) >= 18.5:
             input("How many tickets would you like to buy? ")
+
+        else:
+            print("I am sorry, you cannot buy any ticket.")
+
 
     elif answer == "Tarzan":
         age = input("What is your age? ")
+
+        if int(age) >= 15.5:
             input("How many tickets would you like to buy? ")
+
+        else:
+            print("I am sorry, you cannot buy any ticket.")
+            
 
     elif answer == "Ghost busters":
         age = input("What is your age? ")
+
+        if int(age) >= 12.5:
             input("How many tickets would you like to buy? ")
+
+        else:
+            print("I am sorry, you cannot buy any ticket.")
+

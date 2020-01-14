75528faf05a0b02a9b931a34886364d197b9e680
Zuzana Dostálová <zuzanadostalova@Zuzana-MacBook-Air.local>
2020-01-14 16:40:39 +0100

    ...

--- 7_Cinema_simulator.py
+++ 7_Cinema_simulator.py
@@ -6,72 +6,94 @@ films = {
     "Ghost busters":[12.5]
 }
 
-# print(films["Finding Dory"])
+# # print(films["Finding Dory"])
 
-while True:
-    answer = input("Hello. What movie would you like to see? (Finding Dory, Bourne, Tarzan, or Ghost Busters) ").strip().title()
-    if answer == "Finding Dory":
-        age = input("What is your age? ")  
-        if float(age) >= 3.5:
-            requested_tickets = input("How many tickets would you like to buy? ")
-            tickets = 3
-            if tickets >= int(requested_tickets):
-                print("Thank you for your purchase. Your tickets are on the way.")
+# # My way
+# while True:
+#     answer = input("Hello. What movie would you like to see? (Finding Dory, Bourne, Tarzan, or Ghost Busters) ").strip().title()
+#     if answer == "Finding Dory":
+#         age = input("What is your age? ")  
+#         if float(age) >= 3.5:
+#             requested_tickets = input("How many tickets would you like to buy? ")
+#             tickets = 3
+#             if tickets >= int(requested_tickets):
+#                 print("Thank you for your purchase. Your tickets are on the way.")
             
-            else:
-                print("I am sorry, there are not enough tickets left.")
+#             else:
+#                 print("I am sorry, there are not enough tickets left.")
 
-        else:
-            print("I am sorry, you are not old enough to watch this movie.")
+#         else:
+#             print("I am sorry, you are not old enough to watch this movie.")
 
-    # When doing changes, do not forget to go out of the loop and back into the zsh shell!
+#     # When doing changes, do not forget to go out of the loop and back into the zsh shell!
 
 
-    elif answer == "Bourne":
-        age = input("What is your age? ")
+#     elif answer == "Bourne":
+#         age = input("What is your age? ")
         
-        if float(age) >= 18.5:
-            requested_tickets = input("How many tickets would you like to buy? ")
-            tickets = 20
-            if tickets >= int(requested_tickets):
-                print("Thank you for your purchase. Your tickets are on the way.")
+#         if float(age) >= 18.5:
+#             requested_tickets = input("How many tickets would you like to buy? ")
+#             tickets = 20
+#             if tickets >= int(requested_tickets):
+#                 print("Thank you for your purchase. Your tickets are on the way.")
             
-            else:
-                print("I am sorry, there are not enough tickets left.")
+#             else:
+#                 print("I am sorry, there are not enough tickets left.")
 
 
-        else:
-            print("I am sorry, you are not old enough to watch this movie.")
+#         else:
+#             print("I am sorry, you are not old enough to watch this movie.")
 
 
-    elif answer == "Tarzan":
-        age = input("What is your age? ")
+#     elif answer == "Tarzan":
+#         age = input("What is your age? ")
 
-        if float(age) >= 15.5:
-            requested_tickets = input("How many tickets would you like to buy? ")
-            tickets = 200
-            if tickets >= int(requested_tickets):
-                print("Thank you for your purchase. Your tickets are on the way.")
+#         if float(age) >= 15.5:
+#             requested_tickets = input("How many tickets would you like to buy? ")
+#             tickets = 200
+#             if tickets >= int(requested_tickets):
+#                 print("Thank you for your purchase. Your tickets are on the way.")
             
-            else:
-                print("I am sorry, there are not enough tickets left.")
+#             else:
+#                 print("I am sorry, there are not enough tickets left.")
 
-        else:
-            print("I am sorry, you are not old enough to watch this movie.")
+#         else:
+#             print("I am sorry, you are not old enough to watch this movie.")
 
 
-    elif answer == "Ghost Busters":
-        age = input("What is your age? ")
+#     elif answer == "Ghost Busters":
+#         age = input("What is your age? ")
 
-        if float(age) >= 12.5:
-            requested_tickets = input("How many tickets would you like to buy? ")
-            tickets = 10
-            if tickets >= int(requested_tickets):
-                print("Thank you for your purchase. Your tickets are on the way.")
+#         if float(age) >= 12.5:
+#             requested_tickets = input("How many tickets would you like to buy? ")
+#             tickets = 10
+#             if tickets >= int(requested_tickets):
+#                 print("Thank you for your purchase. Your tickets are on the way.")
             
-            else:
-                print("I am sorry, there are not enough tickets left.")
+#             else:
+#                 print("I am sorry, there are not enough tickets left.")
 
+#         else:
+#             print("I am sorry, you are not old enough to watch this movie.")
+
+# Tutorials way:
+while True:
+
+    choice = input("Hello. What movie would you like to see? ").strip().title()
+
+    if choice in films:
+        age = int(input("What is your age? ").strip())  
+
+        if age >= films[choice][0]:
+            num_seats = films[choice][1]
+
+            if num_seats > 0:
+                print("Enjoy the film.")
+                films[choice][1] = films[choice][1] - 1
+            
         else:
-            print("I am sorry, you are not old enough to watch this movie.")
+            print("You are too young to watch movie.")
+
+# breaks done on line 88
+ 
 

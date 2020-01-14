551a3bbbc5e01bf3897f6c3fca694f42f8a2ca35
Zuzana Dostálová <zuzanadostalova@Zuzana-MacBook-Air.local>
2020-01-14 12:56:45 +0100

    ...

---
+++ 7_Cinema_simulator.py
@@ -0,0 +1,28 @@
+# 45. lesson - Cinema simulator
+films = {
+    "Finding Dory":[3,5],
+    "Bourne":[18,5],
+    "Tarzan":[15,5],
+    "Ghost busters":[12,5]
+}
+
+# print(films["Finding Dory"])
+
+while True:
+    answer = input("Hello. What movie would you like to see? (Finding Dory, Bourne, Tarzan, or Ghost busters) ").strip().title()
+    if answer == "Finding Dory":
+        age = input("What is your age? ")  
+
+        # age_limit = films[0]
+        if age >= str(films["Finding Dory"]):
+            input("How many tickets would you like to buy? ")
+    
+    # if answer == "Bourne":
+    #     age = input("What is your age? ")
+
+    # if answer == "Tarzan":
+    #     age = input("What is your age? ")
+
+    # if answer == "Ghost busters":
+    #     age = input("What is your age? ")
+

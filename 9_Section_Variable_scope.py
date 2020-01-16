5dada88a293a2a91b40060af5a3b5e6b43e52d73
Zuzana Dostálová <zuzanadostalova@dhcp-75-001.eduroam.muni.cz>
2020-01-16 12:13:37 +0100

    ..

---
+++ 9_Section_Variable_scope.py
@@ -0,0 +1,9 @@
+# 58. lesson - Variable scope
+# parts of code not interfering with other parts
+
+# Two types - global scope - global variable, loops and if
+            # local - functions
+
+a = 100
+# f1 - doesnt need parametres - they are optional
+def f1():

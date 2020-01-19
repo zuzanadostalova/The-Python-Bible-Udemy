481f145a8a0e72a62c8d00680976a71333b6b1bd
Zuzana Dostálová <zuzanadostalova@Zuzana-MacBook-Air.local>
2020-01-19 01:44:58 +0100

    ..

--- 10_Section_All_coins.py
+++ 10_Section_All_coins.py
@@ -1,7 +1,81 @@
-# 69. lesson - Make all of the coins
-# polymorphisms
-# abstract classes
+# # 69. lesson - Make all of the coins
+# # polymorphisms
+# # abstract classes
 
+# import random
+
+# class Coin: # general class
+#     def __init__(self, rare=False, clean=True, heads=True, **kwargs): 
+# # packing arguments inside dict kwargs
+
+# # loop over kwargs to define self. is
+#         for key,value in kwargs.items():
+#             setattr(self,key,value)
+# # taking thickness, parameter, flexible...for any dict
+
+#         self.is_rare = rare
+#         self.is_clean = clean
+#         self.heads = heads
+
+#         if self.is_rare:
+#             set.value = self.original_value * 1.25
+#         else:
+#             self.value = self.original_value
+
+#         if self.is_clean:
+#             self.color = self.clean_color
+#         else:
+#             self.color = self.rusty_color
+
+#     def rust(self):
+#         self.color = self.rusty_color # general category - we cannot say greenish
+
+#     def clean(self):
+#         self.color = self.clean_color # cannot say gold
+
+#     # def __del__(self): # destructor
+#     #     print("Coin spent!")
+
+#     def flip(self):
+#         heads_options = [True, False]
+#         choice = random.choice(heads_options)
+#         self.heads = choice
+
+    
+
+
+# class Pound(Coin): # delete pound class; make it inherit to Coin instead
+# # it is going to get all of the methods
+#     def __init__(self):
+#         data = {
+#             "original_value": 1.00,
+#             "clean_color": "gold",
+#             "rusty_color": "greenish",
+#             "num_edges": 1,
+#             "diameter": 22.5,
+#             "thickness": 3.15,
+#             "mass": 9.5
+#         }
+#         super().__init__(**data)
+# using parent class to set up everything else
+# run innit
+# inside init - we get the parent's init = constructer for the data of coins
+# call the generic class to set up everything else
+# we pass data to the parent's innit and let it deal with it - super function it means parent
+# unpacking data from the dict and packing it up above
+
+# one_pound_coin = Pound()
+# print(one_pound_coin.color)
+# # Output:
+# # gold
+
+# one_pound_coin.rust()
+# print(one_pound_coin.color)
+
+# # Output:
+# # greenish
+
+# 70. lesson
 import random
 
 class Coin: # general class
@@ -40,42 +114,6 @@ class Coin: # general class
         heads_options = [True, False]
         choice = random.choice(heads_options)
         self.heads = choice
-
-    
-
-
-class Pound(Coin): # delete pound class; make it inherit to Coin instead
-# it is going to get all of the methods
-    def __init__(self):
-        data = {
-            "original_value": 1.00,
-            "clean_color": "gold",
-            "rusty_color": "greenish",
-            "num_edges": 1,
-            "diameter": 22.5,
-            "thickness": 3.15,
-            "mass": 9.5
-        }
-        super().__init__(**data)
-# using parent class to set up everything else
-# run innit
-# inside init - we get the parent's init = constructer for the data of coins
-# call the generic class to set up everything else
-# we pass data to the parent's innit and let it deal with it - super function it means parent
-# unpacking data from the dict and packing it up above
-
-one_pound_coin = Pound()
-print(one_pound_coin.color)
-# Output:
-# gold
-
-one_pound_coin.rust()
-print(one_pound_coin.color)
-
-# Output:
-# greenish
-
-# 70. lesson
 class One_Pence(Coin):
     def __init__(self):
 
@@ -92,7 +130,6 @@ class One_Pence(Coin):
 
 class Two_Pence(Coin): 
     def __init__(self):
-
         data = {
             "original_value": 0.02,
             "clean_color": "bronze",
@@ -102,10 +139,10 @@ class Two_Pence(Coin):
             "thickness": 1.85,
             "mass": 7.12
         }
+
         super().__init__(**data)
 
-class Five_Pence(Coin): # delete pound class; make it inherit to Coin instead
-# it is going to get all of the methods
+class Five_Pence(Coin): 
     def __init__(self):
 
         data = {
@@ -125,8 +162,7 @@ class Five_Pence(Coin): # delete pound class; make it inherit to Coin instead
         def clean(self): # probably not necessary - inherited
             self.color = self.clean_color
 
-class Ten_Pence(Coin): # delete pound class; make it inherit to Coin instead
-# it is going to get all of the methods
+class Ten_Pence(Coin): 
     def __init__(self):
         data = {
             "original_value": 0.10,
@@ -182,7 +218,7 @@ class Two_Pound(Coin):
     def __init__(self):
         data = {
             "original_value": 0.50,
-            "clean_color": "silver",
+            "clean_color": "gold&silver",
             "rusty_color": "greenish",
             "num_edges": 7,
             "diameter": 27.3,
@@ -195,7 +231,7 @@ coins = [One_Pence(), Two_Pence(), Five_Pence(), Ten_Pence(), Twenty_Pence(), Fi
         One_Pound(), Two_Pound()]
 
 for coin in coins:
-    arguments = [coin, coin.color, coin.value, coin.diameter, coin.thickness, 
-                coin.num_edges, coin.mass]
+    arguments = [coin, coin.color, coin.value, coin.diameter, coin.thickness, coin.num_edges, coin.mass]
 
 string = "{} - Color: {}, value:{}, diameter(mm):{}, thickness(mm):{}, number of edges:{}, mass(g):{}".format(*arguments)
+print(string)
